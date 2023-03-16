16ed5d83fefc4af21c3794e267faca848e9e52a2
Marcelle Moreira <112732876+marcellemoreira@users.noreply.github.com>
2023-03-16 19:23:04 -0300

    Add files via upload

---
+++ AGENDA/templates/header.php
@@ -0,0 +1,33 @@
+<?php
+include_once("config/url.php");
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de contatos</title>
+    <!--BOOSTRAP-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--FONT AWESOME-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--CSS-->
+    <link rel="stylesheet" href="<?= $BASE_URL?>style.css">
+</head>
+<body>
+    <header>
+      <nav class="nav.navbar.navbar-expand-lg.navbar-dark bg-primary">
+        <a  href="<?= $BASE_URL?>index.php">
+            <img class="navbar-brand" src="<?= $BASE_URL?>/img/logo.svg" alt="agenda">
+        </a>
+        <div>
+            <div class="navbar-nav">
+                <a class="nav-link active" id="home-link" href="<?= $BASE_URL?>index.php">Agenda</a>
+                <a class="nav-link active" id="home-link" href="<?= $BASE_URL?>create.php">Adicionar Contato</a>
+            </div>
+        </div>
+    </nav>
+    </header>
